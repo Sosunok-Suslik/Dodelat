1bc5378933318c756166ce10e49b2961fe8fbb88
Suslik Sosinok <nv781467@gmail.com>
2020-01-13 17:08:45 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args ){
+        int kw = 0;
+        System.out.println(kw);
+        String k;
+        String n;
+        String b;
+        System.out.println(k;n;b)
+        if (k){
+            if (k){kw+=0;}
+            else if(n){kw+=1;}
+            else (b) {kw-=1;}
+        }
+        if (n){
+            if (n){kw+=0;}
+            else if(b){kw+=1;}
+            else (k) {kw-=1;}
+            if (b){
+                if (b){kw+=0;}
+                else if(k){kw+=1;}
+                else (n) {kw-=1;}
+                for(int i=1;i<=20;i++);
+
+}
+
